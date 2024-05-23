0865b0642fb762a197a12d20366c740c21d34295
vikasdfghjl <shyvikas63@gmail.com>
2024-05-23 21:33:21 +0530

    dockerfile updated

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
